f67bf4b8af6552ebf23e67ba9e03d02986571b2b
ParadoxChains <sitt.evan@gmail.com>
2020-11-03 23:57:22 +0100

    Rewriting first tests yay

--- C3JI5D-Synthesiser/Google Unit Testing/test.cpp
+++ C3JI5D-Synthesiser/Google Unit Testing/test.cpp
@@ -6,7 +6,15 @@ protected:
 	oscillator osc1;
 };
 
+TEST_F(OscillatorPhaseTest, TestConstructorPhase) {
+	EXPECT_FLOAT_EQ(osc1.getPhase(), 0);
+}
 TEST_F(OscillatorPhaseTest, TestResetPhase) {
 	osc1.resetPhase();
 	EXPECT_FLOAT_EQ(osc1.getPhase(), 0);
+}
+
+TEST_F(OscillatorPhaseTest, TestResetPhaseSpecificValue) {
+	osc1.resetPhase(1);
+	EXPECT_FLOAT_EQ(osc1.getPhase(), 1);
 }
